14e7bdd9a8d9cdf433a5f42d97f6f88f2a8be113
TeaganSmith <teagan.smith@my.utsa.edu>
2025-01-26 02:30:56 -0600

    add congrats page

---
+++ app/congrats.tsx
@@ -0,0 +1,91 @@
+import { Image, StyleSheet, Platform, SafeAreaView, View, Button, ImageBackground, Text } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
+import { router } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+
+
+// 0078D2
+
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaProvider style={{ backgroundColor: '#0078D2' }}>
+      <ImageBackground
+        resizeMode="cover"
+        style={{ flex: 1, width: '100%', height: '100%', justifyContent: 'center' }}
+      >
+        <SafeAreaView style={{ flex: 1, justifyContent: 'space-between' }}>
+          <View style={{ alignItems: 'center', alignContent: 'center', padding: 50, marginTop:150 }}>
+            <Image
+              style={styles.logo}
+              source={require('@/assets/images/LOGO.png')}
+            />
+            <Text style={styles.titleText}>
+              Congratulations on booking with WingIt!
+            </Text>
+            <Text style={styles.titleText}>
+              Let's make it{' '}
+              <Text style={styles.highlightText}>unforgettable</Text>
+            </Text>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    </SafeAreaProvider>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+  logo: {
+    height: 120,
+    width: 265,
+    marginLeft: 65
+  },
+  slogan: {
+    height: 96,
+    width: 500,
+    padding: 5,
+    // marginTop: 20,
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    fontFamily: 'Matcha-EaLge',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#1140FF', // Background color of the button
+    borderRadius: 6, // Rounded corners
+    padding: 5, // Padding around the button
+    margin: 40,
+    marginBottom: 75, // Space around the button
+  },
+  highlightText: {
+    color: '#FF0000', // Red color for "unforgettable"
+    fontWeight: 'bold', // Optional: make it bold for emphasis
+  },
+});
